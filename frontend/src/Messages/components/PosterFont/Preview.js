d8d4e856d614de7de03adfccdd1ece81cdb72a50
Jiannis Sotiropoulos <jiannis@honigstudios.com>
2025-06-08 16:03:24 +0200

    fixed image printing

--- frontend/src/Messages/components/PosterFont/Preview.js
+++ frontend/src/Messages/components/PosterFont/Preview.js
@@ -4,15 +4,16 @@ import styles from "./Preview.module.css";
 
 export default React.forwardRef(function Preview({ header, text }, ref) {
   useLayoutEffect(function() {
-    window.jQuery(ref.current).slabText();
+    window.jQuery('.slab-text-target').slabText();
   });
 
   return (
     <div className={styles.container}>
-      {/* TODO: text and image header should be in the same div */}
-      {header}
-      <div ref={ref} className={`${styles.inner} ${styles.slabText}`}>
-        {text}
+      <div ref={ref}>
+        {header}
+        <div className={`slab-text-target ${styles.slabText}`}>
+          {text}
+        </div>
       </div>
     </div>
   );
